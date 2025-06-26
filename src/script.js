8cf3bb46acbfb88e30a5aa12d36db104ea1fc98f
Busari Shakrudeen <busarishakrullahi007.gmail.com>
2025-06-26 20:49:25 +0100

    Initial commit

--- src/script.js
+++ src/script.js
@@ -15,13 +15,12 @@ const socket = io("http://127.0.0.1:5000");
 
 socket.on("connect", () => {
   console.log(socket.id);
+  socket.on("new-notification", (notification) => {
+    console.log("hello");
+    console.log(notification);
+  });
 });
 console.log(socket);
-
-socket.on("new-notification", (notification) => {
-  console.log("hello");
-  console.log(notification);
-});
 
 // notification logic
 let user;
